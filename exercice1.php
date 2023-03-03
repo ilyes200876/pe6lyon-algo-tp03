1732cff2b87902fdaa15f9ad5275aff2d901ef1d
ilyes200876 <ilyes200876@live.fr>
2023-03-03 15:52:08 +0100

    commit 2

--- exercice1.php
+++ exercice1.php
@@ -1,10 +1,15 @@
 <?php
 
-/*
+/* Votre collègue vous donne la signature suivante :
+FONCTION findValueInTab(var tab, var value) : entier|FAUX
+Cette fonction cherche la valeur value dans le tableau tab, et 
+retourne son indice si elle le trouve, FAUX sinon.
+Coder cette fonction en PHP.
 CN1 : tab [0, 1] , 0 => 0
 CN2 : tab [0, 1] ,1 => 1
 CN3 : tab [0, 1, 3, 6, 5], 6 =>3
-CN4 : tab [0, 1, 3, 6, 5], 4 =>false
+CE1 : tab [0, 1, 3, 6, 5], 4 =>false
+CE2 : tab [], 1 =>false
 
 */
 function findValueInTab($tab, $value)
@@ -45,17 +50,27 @@ function testFindValueInTab_CN3()
     echo "Test de findValueInTab([0, 1, 3, 6, 5], 6) : Réussi " ;
  } 
  else { echo "Test de findValueInTab([0, 1, 3, 6, 5], 6) : Échec " ; 
-} 
+ } 
 }
 
-function testFindValueInTab_CN4()
+function testFindValueInTab_CE1()
 {
   if (findValueInTab([0, 1, 3, 6, 5], 4) == false) { 
     echo "Test de findValueInTab([0, 1, 3, 6, 5], 4) : Réussi " ;
  } 
  else { echo "Test de findValueInTab([0, 1, 3, 6, 5], 4) : Échec " ; 
-} 
+ } 
 }
+
+ function testFindValueInTab_CE2()
+{
+  if (findValueInTab([], 1) == false) { 
+    echo "Test de findValueInTab([], 1) : Réussi " ;
+ } 
+ else { echo "Test de findValueInTab([], 1) : Échec " ; 
+ } 
+} 
+
 testFindValueInTab_CN1() ;
 echo PHP_EOL ;
 
@@ -65,5 +80,8 @@ echo PHP_EOL ;
 testFindValueInTab_CN3( ) ;
 echo PHP_EOL ;
 
-testFindValueInTab_CN4() ;
+testFindValueInTab_CE1() ;
 echo PHP_EOL ;
+
+testFindValueInTab_CE2() ;
+echo PHP_EOL ; 
